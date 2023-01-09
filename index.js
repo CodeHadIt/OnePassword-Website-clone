02463ef465fadadad8da089e203007aa295fb611
CodeHadIt <okinikenneth@gmail.com>
2023-01-10 00:20:46 +0100

    more HTML and JS comments

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// quering everything we need from the Html
+
 const menuBtn = document.querySelector("#mobile-menu");
 const mobileLinks = document.querySelector(".links-container");
 const mobileOnly = document.querySelectorAll(".mobile-only");
 const languages = document.querySelector(".language-container");
 
+//On smaller screens (Mobiles and Tablets), on press of the menu button, we get the navigation links as a drop down menu
 menuBtn.addEventListener("click", ()=> {
     mobileOnly.forEach((link) => {
         link.classList.toggle("displayed");
@@ -10,11 +13,12 @@ menuBtn.addEventListener("click", ()=> {
     mobileLinks.classList.toggle("toggled");
 });
 
-
+//Displaying the options to change language in a pop-up menu
 languages.addEventListener("click", () => {
     languagePopUp();
 })
 
+//the specific function that does so by toggling the "show" class.
 function languagePopUp() {
     let popup = document.querySelector(".popup-container");
     popup.classList.toggle("show");
